ecde6192d49f92357a5a469ad0edc45294ff7ad7
czyt <czyt@w.cn>
2025-04-06 08:57:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# airylark-lzcapp
---
+++ dist/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -a
+if [ ! -f /app/data/env.yaml ];then
+   cp -f /lzcapp/pkg/content/env.yaml /app/data/env.yaml
+fi
+echo "apply env setting"
+source /app/data/env.yaml
+echo "run server"
+node /app/server.js
