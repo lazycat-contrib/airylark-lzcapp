1906d1afe8e1c9c0f170cb90c5e3d4c232dacbc1
czyt <czyt@w.cn>
2025-04-07 21:18:07 +0800

    fix permission

--- dist/startup.sh
+++ dist/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -a
-chown -R nextjs:nodejs /app/node_modules
-if [ ! -f /app/node_modules/data/env.yaml ];then
-   cp -f /lzcapp/pkg/content/env.yaml /app/node_modules/data/env.yaml
+chown -R nextjs:nodejs /app/data
+if [ ! -f /app/data/env.yaml ];then
+   cp -f /lzcapp/pkg/content/env.yaml /app/data/env.yaml
 fi
 echo "apply env setting"
-source /app/node_modules/data/env.yaml
+source /app/data/env.yaml
 set +a
 echo "run server"
 node /app/server.js
